fa32d72ac5df7d75e8f2ea74e3c1ac49ec37c325
jeohong <jeohong59@gmail.com>
2023-04-04 10:04:27 +0900

    [FEAT] 회원가입 스토리보드 수정 및 관련 class 생성

---
+++ HongTalk/ViewController/SignupViewController.h
@@ -0,0 +1,16 @@
+//
+//  SignupViewController.h
+//  HongTalk
+//
+//  Created by 홍정민 on 2023/04/04.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignupViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HongTalk/ViewController/SignupViewController.m
@@ -0,0 +1,19 @@
+//
+//  SignupViewController.m
+//  HongTalk
+//
+//  Created by 홍정민 on 2023/04/04.
+//
+
+#import "SignupViewController.h"
+
+@interface SignupViewController ()
+
+@end
+
+@implementation SignupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+@end
