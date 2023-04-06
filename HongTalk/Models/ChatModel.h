00dff87a62876be4ab0e61dc577c31f8d4daaf0c
jeohong <jeohong59@gmail.com>
2023-04-06 14:39:02 +0900

    [FEAT] ChatModel 작성

---
+++ HongTalk/Models/ChatModel.h
@@ -0,0 +1,36 @@
+//
+//  ChatModel.h
+//  HongTalk
+//
+//  Created by 홍정민 on 2023/04/06.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Comment : NSObject
+
+@property (nonatomic, copy) NSString *uid;
+@property (nonatomic, copy) NSString *message;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+- (NSDictionary *)dictionaryRepresentation;
+
+@end
+
+@interface ChatModel : NSObject
+
+@property (nonatomic, strong) NSMutableDictionary<NSString *, NSNumber *> *users;
+@property (nonatomic, strong) NSMutableDictionary<NSString *, Comment *> *comments;
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+- (NSDictionary *)dictionaryRepresentation;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+/*
+ 
+ */
---
+++ HongTalk/Models/ChatModel.m
@@ -0,0 +1,71 @@
+//
+//  ChatModel.m
+//  HongTalk
+//
+//  Created by 홍정민 on 2023/04/06.
+//
+
+#import "ChatModel.h"
+
+@implementation Comment
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        _uid = dictionary[@"uid"];
+        _message = dictionary[@"message"];
+    }
+    return self;
+}
+
+- (NSDictionary *)dictionaryRepresentation {
+    NSMutableDictionary *dictionary = [NSMutableDictionary new];
+    [dictionary setObject:_uid forKey:@"uid"];
+    [dictionary setObject:_message forKey:@"message"];
+
+    return dictionary;
+}
+
+@end
+
+@implementation ChatModel
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        _users = [NSMutableDictionary new];
+        _comments = [NSMutableDictionary new];
+        NSDictionary *usersDictionary = dictionary[@"users"];
+        for (NSString *userId in usersDictionary) {
+            NSNumber *boolNumber = usersDictionary[userId];
+            [_users setObject:boolNumber forKey:userId];
+        }
+        NSDictionary *commentsDictionary = dictionary[@"comments"];
+        for (NSString *commentId in commentsDictionary) {
+            NSDictionary *commentDictionary = commentsDictionary[commentId];
+            Comment *comment = [[Comment alloc] initWithDictionary:commentDictionary];
+            [_comments setObject:comment forKey:commentId];
+        }
+    }
+    return self;
+}
+
+- (NSDictionary *)dictionaryRepresentation {
+    NSMutableDictionary *dictionary = [NSMutableDictionary new];
+    NSMutableDictionary *usersDictionary = [NSMutableDictionary new];
+    for (NSString *userId in _users) {
+        NSNumber *boolNumber = _users[userId];
+        [usersDictionary setObject:boolNumber forKey:userId];
+    }
+    [dictionary setObject:usersDictionary forKey:@"users"];
+    NSMutableDictionary *commentsDictionary = [NSMutableDictionary new];
+    for (NSString *commentId in _comments) {
+        Comment *comment = _comments[commentId];
+        NSDictionary *commentDictionary = [comment dictionaryRepresentation];
+        [commentsDictionary setObject:commentDictionary forKey:commentId];
+    }
+    [dictionary setObject:commentsDictionary forKey:@"comments"];
+    return dictionary;
+}
+
+@end
