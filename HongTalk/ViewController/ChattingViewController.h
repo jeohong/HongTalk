bec25d7b4621813ec5b4110e2dfe75ebd5f5c172
jeohong <jeohong59@gmail.com>
2023-04-06 10:04:36 +0900

    [FEAT] 메세지 입력시 TextView가 동적으로 늘어나도록 구현

--- HongTalk/ViewController/ChattingViewController.h
+++ HongTalk/ViewController/ChattingViewController.h
@@ -12,6 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ChattingViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UITextView *messageTextView;
 @property (weak, nonatomic) IBOutlet UIButton *sendButton;
+@property (weak, nonatomic) IBOutlet UITableView *chattingTableView;
 
 @end
 
--- HongTalk/ViewController/ChattingViewController.m
+++ HongTalk/ViewController/ChattingViewController.m
@@ -7,7 +7,8 @@
 
 #import "ChattingViewController.h"
 
-@interface ChattingViewController ()
+@interface ChattingViewController ()<UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textviewHeight;
 
 @end
 
@@ -16,9 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [_messageTextView setClipsToBounds: YES];
-    [[_messageTextView layer] setCornerRadius: 10];
-    [[_messageTextView layer] setBorderWidth: 2];
+    [_messageTextView setDelegate: self];
+    [[_messageTextView layer] setBorderWidth: 3];
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+    CGSize size = CGSizeMake(textView.frame.size.width, CGFLOAT_MAX);
+    CGSize newSize = [textView sizeThatFits:size];
+    CGFloat newHeight = MAX(newSize.height, 30); // 최소높이를 30으로 설정합니다.
+    if ( [textView.text componentsSeparatedByString:@"\n"].count >= 5) {
+        self.textviewHeight.constant = 5 * textView.font.lineHeight + textView.textContainerInset.top + textView.textContainerInset.bottom;
+    } else {
+        self.textviewHeight.constant = newHeight;
+    }
+    [self.view layoutIfNeeded];
 }
 
 @end
