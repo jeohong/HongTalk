156a46bf47ee63530a9f37e3c6d45b3cac84121c
jeohong <jeohong59@gmail.com>
2023-04-06 10:15:12 +0900

    [FEAT] 텍스트뷰의 CornerRadius 적용 및 라인수 기준을 '\n' 에서 텍스트뷰의 라인수로 변경

--- HongTalk/ViewController/ChattingViewController.m
+++ HongTalk/ViewController/ChattingViewController.m
@@ -18,14 +18,15 @@
     [super viewDidLoad];
     
     [_messageTextView setDelegate: self];
-    [[_messageTextView layer] setBorderWidth: 3];
+    [[_messageTextView layer] setBorderWidth: 2];
+    [[_messageTextView layer] setCornerRadius: 10];
 }
 
 - (void)textViewDidChange:(UITextView *)textView {
     CGSize size = CGSizeMake(textView.frame.size.width, CGFLOAT_MAX);
     CGSize newSize = [textView sizeThatFits:size];
-    CGFloat newHeight = MAX(newSize.height, 30); // 최소높이를 30으로 설정합니다.
-    if ( [textView.text componentsSeparatedByString:@"\n"].count >= 5) {
+    CGFloat newHeight = MAX(newSize.height, 30);
+    if ( (newSize.height / textView.font.lineHeight) >= 5) {
         self.textviewHeight.constant = 5 * textView.font.lineHeight + textView.textContainerInset.top + textView.textContainerInset.bottom;
     } else {
         self.textviewHeight.constant = newHeight;
