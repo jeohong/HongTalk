5e5c4ac81d28ed465c13d6b4381b2cb77720e7f5
jeohong <jeohong59@gmail.com>
2023-04-07 16:59:05 +0900

    [FEAT] 스토리보드 파일의 IBAction 및 Outlet 연결

--- HongTalk/ViewController/SettingViewController.swift
+++ HongTalk/ViewController/SettingViewController.swift
@@ -8,8 +8,28 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func pressedEditButton(_ sender: Any) {
+        print("프로필 편집")
+    }
+    
+    @IBAction func pressedNotificationSettingButton(_ sender: Any) {
+        print("알림 세팅")
+    }
+    
+    @IBAction func pressedLogoutButton(_ sender: Any) {
+        print("로그아웃")
+    }
+    
+    @IBAction func pressedWithdrawalButton(_ sender: Any) {
+        print("회원탈퇴")
+    }
+    
 }
