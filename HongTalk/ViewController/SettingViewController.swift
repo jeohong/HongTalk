897eb4430f1012544e2648ee2406d4e315f78e09
jeohong <jeohong59@gmail.com>
2023-04-07 16:53:53 +0900

    [FEAT] 세팅 뷰 스토리보드 구현 및 Swift 파일과 연결

---
+++ HongTalk/ViewController/SettingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SettingViewController.swift
+//  HongTalk
+//
+//  Created by 홍정민 on 2023/04/07.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
